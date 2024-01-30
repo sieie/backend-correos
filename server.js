10cbfc4563ccaf1f06f9f833ac0733d60f05ac6e
Prestto03 <sistemas.prestto03@gmail.com>
2024-01-30 13:11:13 -0500

    update de formulario trabaja con nosotros

--- server.js
+++ server.js
@@ -6,23 +6,20 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const path = require('path');
 const mysql = require('mysql2');
+const multer = require('multer');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware para habilitar CORS
 app.use(cors());
-
-// Middleware para analizar el cuerpo de la solicitud en formato JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configuración de conexión a MySQL
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   database: process.env.DB,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
 });
 
 connection.connect((err) => {
@@ -33,58 +30,41 @@ connection.connect((err) => {
   }
 });
 
-
-// // Crear la tabla en MySQL si no existe
-// connection.query(`
-//   CREATE TABLE IF NOT EXISTS datos_formulario (
-//     id INT AUTO_INCREMENT PRIMARY KEY,
-//     nombre VARCHAR(255) NOT NULL,
-//     email VARCHAR(100) NOT NULL,
-//     telefono VARCHAR(10) NOT NULL,
-//     comentario TEXT NOT NULL,
-//     fecha_ingreso TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-//   );
-// `, (err, results) => {
-//   if (err) {
-//     console.error('Error al crear la tabla en MySQL:', err);
-//   }
-// });
-
-
-// Obtener las credenciales desde variables de entorno
 const emailUsuario = process.env.EMAIL_USER_1;
 const contraseñaUsuario = process.env.EMAIL_PASS_1;
 const destinatario = process.env.DESTINATARIO_EMAIL;
+const cc = process.env.CORREOCC;
 
-// Configurar el motor de plantillas EJS
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-// Ruta para procesar el envío de correos
-app.post('/enviar-correo', async (req, res) => {
+const enviarCorreoYGuardarDatos = async (req, res, template, correoCC) => {
   try {
-    const { nombre, telefono, email, comentario } = req.body;
+    console.log(req.body)
+    
+    const { nombre, telefono, email, comentario, divisionEmpresarial, tema, tipo, divisionSeleccionada, ciudad, cv } = req.body;
 
-    // Verificar si el número tiene 10 dígitos y comienza con "09"
     if (telefono.length === 10 && telefono.startsWith("09")) {
-      // Transformar "09" a "+593" y utilizar el resto del número
       const numeroWhatsApp = "+593" + telefono.substring(1);
-
-      // Construye el enlace de WhatsApp
       const enlaceWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent("Hola, veo que estás interesado en...")}`;
-      
-      //Renderizo la plantilla .ejs      
-      const templatePath = path.join(__dirname, 'views/correo-template.ejs');
-      const htmlTemplate = await ejs.renderFile(templatePath, { nombre, telefono, email, comentario, enlaceWhatsApp });
+
+      const templatePath = path.join(__dirname, 'views', template);
+      const htmlTemplate = await ejs.renderFile(templatePath, { nombre, telefono, email, comentario, divisionEmpresarial, enlaceWhatsApp, tema, tipo, divisionSeleccionada, ciudad, cv });
 
       const mailOptions = {
         from: `"Nuevo Cliente" <${email}>`,
         to: destinatario,
+        cc: correoCC,
         subject: "Nuevo Cliente desde la Web",
         html: htmlTemplate,
+        attachments: [
+          {
+            filename: cv.originalname,
+            content: cv.buffer,
+          },
+        ],
       };
 
-      // Configuración del transporte de nodemailer
       const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
@@ -93,25 +73,39 @@ app.post('/enviar-correo', async (req, res) => {
         },
       });
 
-      // Envia el correo electrónico
       const info = await transporter.sendMail(mailOptions);
 
-      // Insertar datos en la tabla de formularios
-      connection.query(
-        'INSERT INTO datos_formulario (nombre, telefono, email, comentario, correo_destino) VALUES (?, ?, ?, ?, ?)',
-        [nombre, telefono, email, comentario, destinatario],
-        (err, results) => {
-          if (err) {
-            console.error('Error al insertar datos en MySQL:', err);
-            res.status(500).json({ mensaje: 'Error al almacenar los datos en la base de datos' });
-          } else {
-            console.log('Datos almacenados en MySQL con éxito:', results);
-            res.status(200).json({ mensaje: 'Correo enviado y datos almacenados con éxito' });
-          }
+      let query = '';
+      let values = [];
+
+      if (req.url === '/enviar-correo/divisiones-empresariales') {
+        query = 'INSERT INTO formulario_divisiones_empresariales (nombre, telefono, email, comentario, correo_destino, division_empresarial) VALUES (?, ?, ?, ?, ?, ?)';
+        values = [nombre, telefono, email, comentario, destinatario, divisionEmpresarial];
+      } else if (req.url === '/enviar-correo/at-cliente') {
+        query = 'INSERT INTO formulario_atencion_clientes (nombre, tema_a_consultar, correo, telefono, comentario) VALUES (?, ?, ?, ?, ?)';
+        values = [nombre, tema, email, telefono, comentario];
+      } else if (req.url === '/enviar-correo/at-proveedor') {
+        query = 'INSERT INTO formulario_atencion_proveedores (nombre, tipo_categoria, correo, telefono, comentario) VALUES (?, ?, ?, ?, ?)';
+        values = [nombre, tipo, email, telefono, comentario];
+      } else if (req.url === '/enviar-correo/responsabilidad-social') {
+        query = 'INSERT INTO formulario_responsabilidad_social (nombre, telefono, correo, comentario) VALUES (?, ?, ?, ?)';
+        values = [nombre, telefono, email, comentario];
+      } else if (req.url === '/enviar-correo/trabaja-nosotros') {
+        query = 'INSERT INTO formulario_trabaja_nosotros (nombre, ciudad, telefono, correo, comentario, divisionSeleccionada, cv) VALUES (?, ?, ?, ?, ?, ?, ?)';
+        values = [nombre, ciudad, telefono, email, comentario, divisionSeleccionada, cv];
+      }
+
+      connection.query(query, values, (err, results) => {
+        if (err) {
+          console.error('Error al insertar datos en MySQL:', err);
+          res.status(500).json({ mensaje: 'Error al almacenar los datos en la base de datos' });
+        } else {
+          console.log('Datos almacenados en MySQL con éxito:', results);
+          res.status(200).json({ mensaje: 'Correo enviado y datos almacenados con éxito' });
         }
-      );
+      });
+
     } else {
-      // No es un número de celular válido, puedes realizar otra acción o simplemente no enviar el correo
       console.log("Número de teléfono no válido");
       res.status(400).json({ mensaje: 'Número de teléfono no válido' });
     }
@@ -119,8 +113,28 @@ app.post('/enviar-correo', async (req, res) => {
     console.error("Error al enviar el correo:", error);
     res.status(500).json({ mensaje: "Error al enviar el correo" });
   }
+};
+
+app.post('/enviar-correo/divisiones-empresariales', async (req, res) => {
+  await enviarCorreoYGuardarDatos(req, res, 'correo-divisiones.ejs');
 });
 
+app.post('/enviar-correo/at-cliente', async (req, res) => {
+  await enviarCorreoYGuardarDatos(req, res, 'at-cliente.ejs');
+});
+
+app.post('/enviar-correo/at-proveedor', async (req, res) => {
+  await enviarCorreoYGuardarDatos(req, res, 'at-proveedores.ejs');
+});
+
+app.post('/enviar-correo/responsabilidad-social', async (req, res) => {
+  await enviarCorreoYGuardarDatos(req, res, 'responsabilidad-social.ejs');
+});
+
+app.post('/enviar-correo/trabaja-nosotros', upload.single('cv'), async (req, res) => {
+  const correoCC = cc;
+  await enviarCorreoYGuardarDatos(req, res, 'trabaja-nosotros.ejs', correoCC);
+});
 
 app.listen(PORT, () => {
   console.log(`Backend iniciado en http://localhost:${PORT}`);
